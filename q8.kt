59e9d326d1fc10ad574abdd643be714a55018b7f
Debasrita Das <cst.23bcta65@silicon.ac.in>
2026-05-30 14:40:56 +0530

    Added q8

---
+++ q8.kt
@@ -0,0 +1,8 @@
+//Write an object Database with a fun connect() that prints "Connected to DB"
+object Database {
+    fun connect() { println("Connected to DB") }
+}
+
+fun main() {
+    Database.connect()
+}
